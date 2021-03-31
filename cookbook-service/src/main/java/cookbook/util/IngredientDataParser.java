56517b92c9a2f4291d71c09766fdb4a4756c3821
Németh Bálint <balint.nemeth@futureofmedia.hu>
2021-03-31 21:52:17 +0200

    IngredientDataParser

---
+++ cookbook-service/src/main/java/cookbook/util/IngredientDataParser.java
@@ -0,0 +1,32 @@
+package cookbook.util;
+
+import cookbook.domain.Ingredient;
+import cookbook.domain.Unit;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Component
+public class IngredientDataParser implements DataParser<Ingredient> {
+
+    @Override
+    public List<Ingredient> parse(String text) {
+        return Arrays.stream(text.split("\r\n"))
+                .filter(x -> !x.isEmpty())
+                .map(line -> {
+                    String[] values = line.split(" ");
+
+                    double amount = Double.parseDouble(values[0]);
+                    String name = values[2];
+                    Unit unit = Unit.valueOf(values[1]);
+
+                    return new Ingredient(
+                            amount,
+                            name,
+                            unit);
+                })
+                .collect(Collectors.toList());
+    }
+}
