01508e125e15a8cc4bf7df13f3f59ded1fd799f4
Németh Bálint <balint.nemeth@futureofmedia.hu>
2021-03-31 13:09:48 +0200

    User model

---
+++ cookbook-domain/src/main/java/cookbook/domain/User.java
@@ -0,0 +1,26 @@
+package cookbook.domain;
+
+public abstract class User {
+
+    private long id;
+    private String username;
+    private String password;
+
+    protected User(long id, String username, String password) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+}
