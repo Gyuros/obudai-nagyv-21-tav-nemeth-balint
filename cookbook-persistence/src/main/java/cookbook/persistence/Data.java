1196e76c6f2281845d24aab6ee580e854a8d7b6a
Németh Bálint <balint.nemeth@futureofmedia.hu>
2021-03-31 14:28:47 +0200

    TxtData updated

--- cookbook-persistence/src/main/java/cookbook/persistence/Data.java
+++ cookbook-persistence/src/main/java/cookbook/persistence/Data.java
@@ -1,8 +1,10 @@
 package cookbook.persistence;
 
+import java.io.IOException;
+
 public interface Data {
 
-    String read(String filename);
+    String read(String filename) throws IOException;
 
-    void write(String filename, String value);
+    void write(String filename, String value) throws IOException;
 }
