58b4b423a624a202e7340272f43dbf0eeadcd05a
Németh Bálint <balint.nemeth@futureofmedia.hu>
2021-04-06 12:46:18 +0200

    Comment service saveComment implemented

--- cookbook-service/src/main/java/cookbook/service/comment/CookbookCommentService.java
+++ cookbook-service/src/main/java/cookbook/service/comment/CookbookCommentService.java
@@ -3,12 +3,19 @@ package cookbook.service.comment;
 import cookbook.domain.Comment;
 import cookbook.domain.Recipe;
 import cookbook.service.CookbookBaseService;
+import cookbook.service.cook.CookbookCookService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
 
 @Component
 public class CookbookCommentService extends CookbookBaseService<Comment> implements CommentService {
 
-    public static final String FILE_NAME = "comments.txt";
+    private static final String FILE_NAME = "comments.txt";
+
+    @Autowired
+    private CookbookCookService cookService;
 
     public CookbookCommentService() {
         super(FILE_NAME);
@@ -16,6 +23,18 @@ public class CookbookCommentService extends CookbookBaseService<Comment> impleme
 
     @Override
     public void saveComment(Recipe recipe, String comment) {
+        long lastId = models.stream()
+                .map(Comment::getId)
+                .max(Long::compare)
+                .orElse(1L);
 
+        Comment newComment = new Comment(
+                lastId + 1,
+                comment,
+                LocalDateTime.now(),
+                cookService.getCurrentUser(),
+                recipe);
+
+        models.add(newComment);
     }
 }
