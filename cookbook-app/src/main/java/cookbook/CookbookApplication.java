b1a4adecd20c4ff824274820688e4556aa6b8ea4
Németh Bálint <balint.nemeth@futureofmedia.hu>
2021-04-26 14:45:54 +0200

    Webapp hello world

--- cookbook-app/src/main/java/cookbook/CookbookApplication.java
+++ cookbook-app/src/main/java/cookbook/CookbookApplication.java
@@ -1,13 +1,12 @@
 package cookbook;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
+@Configuration
 public class CookbookApplication {
 
     @Autowired
@@ -17,10 +16,10 @@ public class CookbookApplication {
         SpringApplication.run(CookbookApplication.class, args);
     }
 
-    @Bean
-    public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
-        return args -> {
-            app.start();
-        };
-    }
+//    @Bean
+//    public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+//        return args -> {
+//            app.start();
+//        };
+//    }
 }
---
+++ cookbook-app/src/main/webapp/WEB-INF/views/hello.jsp
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<meta charset="UTF-8">
+<title>Cookbook - Hello</title>
+<meta name="viewport" content="width=device-width,initial-scale=1">
+<link rel="stylesheet" href="">
+<script src=""></script>
+<body>
+
+<h1>Hello ${name}</h1>
+
+</body>
+</html>
